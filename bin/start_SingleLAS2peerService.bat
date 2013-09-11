5badc6f75be14361440a34645c65180c104e44b7
Gordin <9ordin@gmail.com>
2013-09-11 16:32:40 +0200

    fixed paths to agent-files in scripts

--- bin/start_SingleLAS2peerService.bat
+++ bin/start_SingleLAS2peerService.bat
@@ -3,5 +3,5 @@ cd ..
 set BASE=%CD%
 set CLASSPATH="%BASE%/lib/*;"
 
-java -cp %CLASSPATH% i5.las2peer.tools.ServiceStarter -x startup/agent-SampleService.xml SampleServicePass -b ""
+java -cp %CLASSPATH% i5.las2peer.tools.ServiceStarter -x startup/agent-service-i5.las2peer.ServicePackage.xml SampleServicePass -b ""
 pause
--- bin/start_SingleLAS2peerService.sh
+++ bin/start_SingleLAS2peerService.sh
@@ -3,4 +3,4 @@
 BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../
 export CLASSPATH="${BASE}lib/*"
 
-java -cp "${CLASSPATH}" i5.las2peer.tools.ServiceStarter -x startup/agent-SampleService.xml SampleServicePass -b ""
+java -cp "${CLASSPATH}" i5.las2peer.tools.ServiceStarter -x startup/agent-service-i5.las2peer.ServicePackage.xml SampleServicePass -b ""
