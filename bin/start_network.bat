9878876cb0f057456ca9c8df9d2e96e89c07e50f
Peter de Lange <peter.de.lange@rwth-aachen.de>
2014-04-16 14:06:55 +0200

    small windows script improvement

--- bin/start_network.bat
+++ bin/start_network.bat
@@ -3,5 +3,5 @@ cd ..
 set BASE=%CD%
 set CLASSPATH="%BASE%/lib/*;"
 
-java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher -s 9001 - interactive
+java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher windows_shell -s 9001 - interactive
 pause
