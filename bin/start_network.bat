ed440a1e1492134023bf4b38fc55f195b2ff4f6b
Thomas Cujé <tcuje@freenet.de>
2014-05-22 19:53:33 +0200

    improved start_network scripts, see LAS-50 for details

--- bin/start_network.bat
+++ bin/start_network.bat
@@ -1,7 +1,6 @@
-cd %~dp0
-cd ..
-set BASE=%CD%
-set CLASSPATH="%BASE%/lib/*;"
+:: this script starts a las2peer node providing the example service of this project
+:: pls execute it from the bin folder of your deployment by double-clicking on it
 
-java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher -s 9001 - interactive
+cd ..
+java -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher windows_shell -s 9011 - uploadStartupDirectory('etc/startup') startService('i5.las2peer.services.servicePackage.ServiceClass','SampleServicePass') startWebConnector interactive
 pause
--- bin/start_network.sh
+++ bin/start_network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# this script starts a las2peer node
-# pls run the script form the root folder of your deployment, e. g. ./bin/start_network.sh
+# this script starts a las2peer node providing the example service of this project
+# pls execute it from the root folder of your deployment, e. g. ./bin/start_network.sh
 
-java -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -s 9011 - interactive
+java -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -s 9011 - uploadStartupDirectory\(\'etc/startup\'\) startService\(\'i5.las2peer.services.servicePackage.ServiceClass\',\'SampleServicePass\'\) startWebConnector interactive
