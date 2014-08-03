49804aee26c845c0ccb96cf900365a4e652731e8
Peter de Lange <peter.de.lange@rwth-aachen.de>
2014-08-03 15:31:21 +0200

    fixed windows start script (classpath problems)

--- bin/start_network.bat
+++ bin/start_network.bat
@@ -2,5 +2,8 @@
 :: pls execute it from the bin folder of your deployment by double-clicking on it
 
 cd ..
-java -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher windows_shell -s 9011 - uploadStartupDirectory('etc/startup') startService('i5.las2peer.services.servicePackage.ServiceClass','SampleServicePass') startWebConnector interactive
+set BASE=%CD%
+set CLASSPATH="%BASE%/lib/*;"
+
+java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher windows_shell -s 9011 - uploadStartupDirectory('etc/startup') startService('i5.las2peer.services.servicePackage.ServiceClass','SampleServicePass') startWebConnector interactive
 pause
