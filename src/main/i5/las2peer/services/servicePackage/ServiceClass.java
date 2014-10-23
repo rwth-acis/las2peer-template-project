a59e2e25faf1a1726a3317831bcf38c1dcc5cff5
Thomas Cujé <tcuje@freenet.de>
2014-10-23 22:00:20 +0200

    changed default target to all added three example user moved database config to service class properties upgraded to latest LAS2peer and WebConnector snapshots added node-storage to gitignore enabled anonymous access for WebConnector added example functions to access database

--- src/main/i5/las2peer/services/servicePackage/ServiceClass.java
+++ src/main/i5/las2peer/services/servicePackage/ServiceClass.java
@@ -9,42 +9,171 @@ import i5.las2peer.restMapper.annotations.PathParam;
 import i5.las2peer.restMapper.annotations.Version;
 import i5.las2peer.restMapper.tools.ValidationResult;
 import i5.las2peer.restMapper.tools.XMLCheck;
+import i5.las2peer.security.Context;
 import i5.las2peer.security.UserAgent;
+import i5.las2peer.services.servicePackage.database.DatabaseManager;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
- * 
  * LAS2peer Service
  * 
  * This is a template for a very basic LAS2peer service
  * that uses the LAS2peer Web-Connector for RESTful access to it.
  * 
- * 
- *
  */
 @Path("example")
 @Version("0.1")
 public class ServiceClass extends Service {
 
+	private String jdbcDriverClassName;
+	private String jdbcLogin;
+	private String jdbcPass;
+	private String jdbcUrl;
+	private String jdbcSchema;
+	private DatabaseManager dbm;
 
+	public ServiceClass() {
+		// read and set properties values
+		// IF THE SERVICE CLASS NAME IS CHANGED, THE PROPERTIES FILE NAME NEED TO BE CHANGED TOO!
+		setFieldValues();
+		// instantiate a database manager to handle database connection pooling and credentials
+		dbm = new DatabaseManager(jdbcDriverClassName, jdbcLogin, jdbcPass, jdbcUrl, jdbcSchema);
+	}
 
 	/**
-	 * This method is needed for every RESTful application in LAS2peer.
+	 * Simple function to validate a user login.
+	 * Basically it only serves as a "calling point" and does not really validate a user
+	 * (since this is done previously by LAS2peer itself, the user does not reach this method
+	 * if he or she is not authenticated).
 	 * 
-	 * @return the mapping
 	 */
-    public String getRESTMapping()
-    {
-        String result="";
-        try {
-            result= RESTMapper.getMethodsAsXML(this.getClass());
-        } catch (Exception e) {
+	@GET
+	@Path("validate")
+	public String validateLogin() {
+		String returnString = "";
+		returnString += "You are " + ((UserAgent) getActiveAgent()).getLoginName() + " and your login is valid!";
+		return returnString;
+	}
 
-            e.printStackTrace();
-        }
-        return result;
-    }
+	/**
+	 * Another example method.
+	 * 
+	 * @param myInput
+	 * 
+	 */
+	@POST
+	@Path("myMethodPath/{input}")
+	public String exampleMethod(@PathParam("input") String myInput) {
+		String returnString = "";
+		returnString += "You have entered " + myInput + "!";
+		return returnString;
+	}
+
+	/**
+	 * Example method that shows how to retrieve a user email address from a database.
+	 * 
+	 * @param username
+	 * @return The email address for the given username or an error message
+	 */
+	@GET
+	@Path("getUserEmail/{username}")
+	public String getUserEmail(@PathParam("username") String username) {
+		String result = "";
+		Connection conn = null;
+		PreparedStatement stmnt = null;
+		ResultSet rs = null;
+		try {
+			conn = dbm.getConnection();
+			stmnt = conn.prepareStatement("SELECT email FROM users WHERE username = ?;");
+			stmnt.setString(1, username);
+			rs = stmnt.executeQuery();
+			if (rs.next()) {
+				result = rs.getString(1);
+			} else {
+				result = "No result for username " + username;
+			}
+		} catch (Exception e) {
+			result = "Error occurred: " + e;
+		} finally {
+			// free resources if exception or not
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e) {
+					Context.logError(this, e.getMessage());
+				}
+			}
+			if (stmnt != null) {
+				try {
+					stmnt.close();
+				} catch (Exception e) {
+					Context.logError(this, e.getMessage());
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (Exception e) {
+					Context.logError(this, e.getMessage());
+				}
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Example method that shows how to change a user email address in a database.
+	 * 
+	 * @param username
+	 * @param email
+	 * @return Returns a message with the number of rows that were updated or an error message.
+	 */
+	@POST
+	@Path("setUserEmail/{username}/{email}")
+	public String setUserEmail(@PathParam("username") String username, @PathParam("email") String email) {
+		String result = "";
+		Connection conn = null;
+		PreparedStatement stmnt = null;
+		ResultSet rs = null;
+		try {
+			conn = dbm.getConnection();
+			stmnt = conn.prepareStatement("UPDATE users SET email = ? WHERE username = ?;");
+			stmnt.setString(1, email);
+			stmnt.setString(2, username);
+			int rows = stmnt.executeUpdate(); // same works for insert
+			result = "Database updated. " + rows + " rows affected";
+		} catch (Exception e) {
+			result = "Error occurred: " + e;
+		} finally {
+			// free resources if exception or not
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e) {
+					Context.logError(this, e.getMessage());
+				}
+			}
+			if (stmnt != null) {
+				try {
+					stmnt.close();
+				} catch (Exception e) {
+					Context.logError(this, e.getMessage());
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (Exception e) {
+					Context.logError(this, e.getMessage());
+				}
+			}
+		}
+		return result;
+	}
 
 	/**
 	 * Method for debugging purposes.
@@ -55,55 +184,38 @@ public class ServiceClass extends Service {
 	 * To avoid being overlooked/ignored the method is implemented here and not in the test section.
 	 * @return  true, if mapping correct
 	 */
-    public boolean debugMapping()
-	{
+	public boolean debugMapping() {
 		String XML_LOCATION = "./restMapping.xml";
-		String xml= getRESTMapping();
+		String xml = getRESTMapping();
 
-		try{
-			RESTMapper.writeFile(XML_LOCATION,xml);
-		}catch (IOException e){
+		try {
+			RESTMapper.writeFile(XML_LOCATION, xml);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		XMLCheck validator= new XMLCheck();
+		XMLCheck validator = new XMLCheck();
 		ValidationResult result = validator.validate(xml);
 
-		if(result.isValid())
+		if (result.isValid())
 			return true;
 		return false;
 	}
-    
-    /**
-     * 
-     * Simple function to validate a user login.
-     * Basically it only serves as a "calling point" and does not really validate a user
-     * (since this is done previously by LAS2peer itself, the user does not reach this method
-     * if he or she is not authenticated).
-     * 
-     */
-    @GET
-    @Path("validate")
-    public String validateLogin()
-    {
-    	String returnString = "";
-    	returnString += "You are " + ((UserAgent) getActiveAgent()).getLoginName() + " and your login is valid!";
-    	return returnString;
-    }
-    
-    /**
-     * 
-     * Another example method.
-     * 
-     * @param myInput
-     * 
-     */
-    @POST
-    @Path("myMethodPath/{input}")
-    public String exampleMethod( @PathParam("input") String myInput)
-    {
-    	String returnString = "";
-    	returnString += "You have entered " + myInput + "!";
-    	return returnString;
-    }
+
+	/**
+	 * This method is needed for every RESTful application in LAS2peer. There is no need to change!
+	 * 
+	 * @return the mapping
+	 */
+	public String getRESTMapping() {
+		String result = "";
+		try {
+			result = RESTMapper.getMethodsAsXML(this.getClass());
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		return result;
+	}
+
 }
--- src/main/i5/las2peer/services/servicePackage/database/DatabaseManager.java
+++ src/main/i5/las2peer/services/servicePackage/database/DatabaseManager.java
@@ -1,10 +1,6 @@
 package i5.las2peer.services.servicePackage.database;
 
-import i5.las2peer.api.Configurable;
-
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -13,24 +9,12 @@ import org.apache.commons.dbcp2.BasicDataSource;
  * This class manages database credentials and provides connection from a connection pooling system
  *
  */
-public class DatabaseManager extends Configurable {
+public class DatabaseManager {
 
 	private static BasicDataSource dataSource;
-	private String jdbcDriverClassName;
-	private String jdbcLogin;
-	private String jdbcPass;
-	private String jdbcUrl;
-	private String jdbcSchema;
-	private final int DEFAULT_JDBC_DEFAULT_QUERY_TIMEOUT = 1000;
-	private int jdbcDefaultQueryTimeout = DEFAULT_JDBC_DEFAULT_QUERY_TIMEOUT;
-	private final int DEFAULT_JDBC_MAC_CONN_LIFETIME_MILLIS = 100000;
-	private int jdbcMaxConnLifetimeMillis = DEFAULT_JDBC_MAC_CONN_LIFETIME_MILLIS;
 
-	public DatabaseManager() throws ClassNotFoundException {
-		// read and set properties values
-		setFieldValues();
-		// request classloader to load JDBC driver class
-		Class.forName(jdbcDriverClassName);
+	public DatabaseManager(String jdbcDriverClassName, String jdbcLogin, String jdbcPass, String jdbcUrl,
+			String jdbcSchema) {
 		// prepare and configure data source
 		dataSource = new BasicDataSource();
 		dataSource.setDefaultAutoCommit(true);
@@ -39,90 +23,12 @@ public class DatabaseManager extends Configurable {
 		dataSource.setPassword(jdbcPass);
 		dataSource.setUrl(jdbcUrl + jdbcSchema);
 		dataSource.setValidationQuery("SELECT 1");
-		dataSource.setDefaultQueryTimeout(jdbcDefaultQueryTimeout);
-		dataSource.setMaxConnLifetimeMillis(jdbcMaxConnLifetimeMillis);
+		dataSource.setDefaultQueryTimeout(1000);
+		dataSource.setMaxConnLifetimeMillis(100000);
 	}
 
 	public Connection getConnection() throws SQLException {
 		return dataSource.getConnection();
-	}
-
-	public static void main(String[] args) throws Exception {
-		DatabaseManager db = new DatabaseManager();
-		// select example
-		Connection conn = null;
-		PreparedStatement stmnt = null;
-		ResultSet rs = null;
-		try {
-			conn = db.getConnection();
-			stmnt = conn.prepareStatement("SELECT email FROM users WHERE username = ?");
-			stmnt.setString(1, "userA");
-			rs = stmnt.executeQuery();
-			if (rs.isBeforeFirst()) {
-				System.out.println("No entry found for userA");
-			} else {
-				System.out.println("Email for userA is " + rs.getString(0));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			// free resources if exception or not
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			if (stmnt != null) {
-				try {
-					stmnt.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			if (conn != null) {
-				try {
-					conn.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		// update example
-		try {
-			conn = db.getConnection();
-			stmnt = conn.prepareStatement("UPDATE users SET email = ? WHERE username = ?");
-			stmnt.setString(1, "userA@example.org");
-			stmnt.setString(2, "userA");
-			int rows = stmnt.executeUpdate(); // same works for insert
-			System.out.println("Database updated. "+rows+" rows affected");
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			// free resources if exception or not
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			if (stmnt != null) {
-				try {
-					stmnt.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			if (conn != null) {
-				try {
-					conn.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
 	}
 
 }
