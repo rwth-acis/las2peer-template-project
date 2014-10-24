5d2b9a7dfae0e900859909ab96ce9e73c17fa08c
Thomas Cujé <tcuje@freenet.de>
2014-10-24 10:28:02 +0200

    added changing attribute to snapshot dependencies changed startup scripts to LAS2peer 0.3 syntax

--- bin/start_UserAgentGenerator.bat
+++ bin/start_UserAgentGenerator.bat
@@ -3,5 +3,5 @@ cd ..
 set BASE=%CD%
 set CLASSPATH="%BASE%/lib/*"
 
-java -cp %CLASSPATH% i5.las2peer.tools.UserAgentGenerator userAPass "User A" usera@mail.com
+java -cp %CLASSPATH% i5.las2peer.tools.UserAgentGenerator userAgentPassword "Username" useremail@example.org
 pause
--- bin/start_network.bat
+++ bin/start_network.bat
@@ -5,5 +5,5 @@ cd ..
 set BASE=%CD%
 set CLASSPATH="%BASE%/lib/*;"
 
-java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher windows_shell -s 9011 - uploadStartupDirectory('etc/startup') startService('i5.las2peer.services.servicePackage.ServiceClass','SampleServicePass') startWebConnector interactive
+java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher -w -p 9011 uploadStartupDirectory('etc/startup') startService('i5.las2peer.services.servicePackage.ServiceClass','SampleServicePass') startWebConnector interactive
 pause
